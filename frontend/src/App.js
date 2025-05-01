bd84342734e4cb3e2241e043371f292ffc5118f9
liamhorton0528 <n01549257@humber.ca>
2025-05-01 19:50:18 -0400

    adding some page transition animation + spinning react logo  for all pages.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
+
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -7,17 +9,27 @@ import Education from './pages/Education';
 import Navbar from './components/Navbar';
 import Contact from './pages/Contact';
 
-function App() {
+function AnimatedRoutes() {
+  const location = useLocation();
+
   return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
           <Route path="/" element={<Home />} />
           <Route path="/About" element={<About />} />
           <Route path="/Education" element={<Education />} />
           <Route path="/Contact" element={<Contact />} />
         </Routes>
+    </AnimatePresence>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <AnimatedRoutes />
         <Footer />
       </Router>
     </div>
--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -1,5 +1,13 @@
+import { motion } from "framer-motion";
+
 export default function Contact()  {
-    return <div className="page-content">
+    return <motion.div className="page-content"
+    initial={{opacity:0, y:20}}
+    animate={{opacity: 1, y: 0}}
+    exit={{opacity: 0, y: -20}}
+    transition={{duration: 0.6, ease: "easeInOut"}}
+    >
         <h2>Contact Page</h2>
-    </div>
+        <img className="App-logo" src="./logo512.png" style={{height: '300px', width: '300px'}}/>
+    </motion.div>
 }
