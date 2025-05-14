cd544c2efcb585f3041e16741c704631580d11f1
liamhorton0528 <n01549257@humber.ca>
2025-05-14 15:23:33 -0400

    typo fix

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -44,7 +44,7 @@ export default function Home() {
            aesthethically pleasing and responsive interfaces, increasingly efficient and fast server-side code, 
            stronger security implementations, and learning how to apply these skills and more in new languages.<br/><br/>
            A goal that is a bit more relevant to me right now is finding a job. I would really like to focus on web development as 
-           I mentioned above, however as long as I'm coding I know I will be happy. What I love above this line of work is that we are 
+           I mentioned above, however as long as I'm coding I know I will be happy. What I love about this line of work is that we are 
            constantly learning and working to improve ourselves, not just because of the need to but because there are new and exciting 
            things being created everyday that continue to keep things interesting.<br/><br/>
            A goal I have for the future is to develop a very strong understanding of networking, hacking, and security concepts/practices. There is so much 
